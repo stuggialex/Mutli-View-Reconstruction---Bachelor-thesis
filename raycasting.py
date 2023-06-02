dfa6194f76c8e6700985bf218faa35252c22bb04
stuggialex <63969996+stuggialex@users.noreply.github.com>
2023-06-02 18:38:13 +0200

    cleaned up the code

--- raycasting.py
+++ raycasting.py
@@ -1,15 +1,12 @@
 import json
+import torch
 from mpl_toolkits import mplot3d
-
-%matplotlib inline
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 fig = plt.figure()
-ax = plt.axes(projection='3d')
-
-
+ax = fig.add_subplot(projection='3d')
 
 """
 Simple raycasting
@@ -20,35 +17,62 @@ Simple raycasting
 
 """
 
-"""
- The first 3 columns are the +X, +Y, and +Z defining the camera orientation, and the X, Y, Z values define the origin. 
- The last row is to be compatible with homogeneous coordinates.
-         [+X0 +Y0 +Z0 X]
-         [+X1 +Y1 +Z1 Y]
-         [+X2 +Y2 +Z2 Z]
-         [0.0 0.0 0.0 1]
+def get_camera (n):
+    """
+    Gets the transform matrix for the first n cameras from the dataset
 
-work with pytorch tensor
-"""
+    Args:
+        n: int
 
-"""
-function that gets the transform matrix for one camera from the dataset
-"""
-def get_camera ():
+    Returns:
+        tensor with the camera data
+    """
+    
     with open("quadrants.json", "r") as read_file:
         cameras = json.load(read_file)
-    camera_arr = cameras['frames'][0]['transform_matrix']
-    return camera_arr
+    camera_arr = cameras['frames'][n]['transform_matrix']
+    return torch.tensor(camera_arr)
 
-def get_camera_origin(camera):
-    arr = []
-    for x in range(3):
-        arr.append(camera[x][3])
-    return arr
+def get_rotation_matrix(camera):
+    return camera[:3, :3]
 
-print(get_camera())
+def calculate_viewing_direction(camera_view):
+    """
+    Calculates the viewing direction of a camera
+    with matrix multiplication 
+
+    Args:
+        camera_view:3x3 tensor
+
+    Returns:
+        3x3 tensor
+    """
+
+    null_vec = torch.tensor([0, 0, 1], dtype=torch.float32)
+    return torch.matmul(get_rotation_matrix(camera_view), null_vec * -1)
 
 def visualize_camera(camera):
+    """
+    Args:
+        camera:4x4 tensor
+    """
     x = camera[0][3]
     y = camera[1][3]
     z = camera[2][3]
+    u, v, w = calculate_viewing_direction(camera)
+    ax.quiver(x, y, z, u, v, w, length=2, normalize=True)
+
+for n in range(5):
+    visualize_camera(get_camera(n))
+
+# Set the labels of the axes
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+# Set the x, y, and z limits
+ax.set_xlim(-4, 4)
+ax.set_ylim(-4, 4)
+ax.set_zlim(-4, 4)
+
+plt.show()
