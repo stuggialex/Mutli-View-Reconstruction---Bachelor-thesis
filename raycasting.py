c1e1be395e962883b937b15e9d5b926a477ec05b
stuggialex <63969996+stuggialex@users.noreply.github.com>
2023-06-09 10:24:56 +0200

    Added Raycasting

--- raycasting.py
+++ raycasting.py
@@ -1,4 +1,5 @@
 import json
+import random
 import torch
 from mpl_toolkits import mplot3d
 
@@ -8,30 +9,32 @@ import matplotlib.pyplot as plt
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
+IMG_HEIGHT = 800
+IMG_WIDTH = 800
+RATIO = IMG_WIDTH / IMG_HEIGHT
+
 """
 Simple raycasting
 
 1. From one camera send out ray(s) #check out the data, how do they represent the camera
 2. Sample points along the ray(s)
 3. compare the sampled points with other cameras
-
 """
 
-def get_camera (n):
+def get_camera_data ():
     """
-    Gets the transform matrix for the first n cameras from the dataset
-
-    Args:
-        n: int
+    Gets the camera angle for the intrinsic matrix
+    aswell as the transform matrices for the cameras from the dataset
 
     Returns:
-        tensor with the camera data
+        array
+        float
     """
     
     with open("quadrants.json", "r") as read_file:
         cameras = json.load(read_file)
-    camera_arr = cameras['frames'][n]['transform_matrix']
-    return torch.tensor(camera_arr)
+    camera_array = cameras['frames']
+    return camera_array, cameras['camera_angle_x']
 
 def get_rotation_matrix(camera):
     return camera[:3, :3]
@@ -61,9 +64,49 @@ def visualize_camera(camera):
     z = camera[2][3]
     u, v, w = calculate_viewing_direction(camera)
     ax.quiver(x, y, z, u, v, w, length=2, normalize=True)
+    
 
+def calculate_intrinsic(camera_angle_x):
+    """
+    """
+    camera_angle_y = RATIO * camera_angle_x
+    intrinsic = torch.tensor(np.zeros(shape=(3, 3), dtype=np.float32))
+    focal_x = (IMG_WIDTH / 2) / np.tan(camera_angle_x / 2)
+    focal_y = (IMG_HEIGHT / 2) / np.tan(camera_angle_y / 2)
+    intrinsic[0][0] = focal_x
+    intrinsic[1][1] = focal_y
+    intrinsic[2][2] = 1
+    return intrinsic
+
+def get_rays_np(H, W, focal, c2w):
+    """
+    Get ray origins, directions from a pinhole camera.
+    """
+    i, j = np.meshgrid(np.arange(W, dtype=np.float32),
+                       np.arange(H, dtype=np.float32), indexing='xy')
+    dirs = np.stack([(i-W*.5)/focal, -(j-H*.5)/focal, -np.ones_like(i)], -1)
+    rays_d = np.sum(dirs[..., np.newaxis, :] * c2w[:3, :3], -1)
+    rays_o = np.broadcast_to(c2w[:3, -1], np.shape(rays_d))
+    return rays_o, rays_d
+
+def visualize_rays(focal, c2w):
+    """
+    Args:
+    focal length of the camera
+    transformation matrix of the camera
+    """
+    rays_o, rays_d = get_rays_np(IMG_HEIGHT, IMG_WIDTH, focal, torch.tensor(c2w).numpy())
+    for m in range(1, 801,100):
+        for n in range(1, 801,100):
+            ax.quiver(rays_o[m][n][0], rays_o[m][n][1], rays_o[m][n][2], rays_d[m][n][0], rays_d[m][n][1], rays_d[m][n][2], length=5, normalize=True)
+
+
+camera_data, camera_angle_x = get_camera_data()
 for n in range(5):
-    visualize_camera(get_camera(n))
+    camera = camera_data[n]['transform_matrix']
+    intrinsic = calculate_intrinsic(camera_angle_x)
+    #visualize_rays(intrinsic[0][0], camera)
+    visualize_camera(torch.tensor(camera))
 
 # Set the labels of the axes
 ax.set_xlabel('X')
@@ -74,5 +117,4 @@ ax.set_zlabel('Z')
 ax.set_xlim(-4, 4)
 ax.set_ylim(-4, 4)
 ax.set_zlim(-4, 4)
-
 plt.show()
